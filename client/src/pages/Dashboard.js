96d58e196215ae0a71b3a430c584946f390bfa5c
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-08-25 11:52:34 +0100

    Edited styling - some are not loading correctly

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,108 +1,130 @@
-'use client '
-import { Box, Typography, AppBar, Toolbar, Grid, Card, CardContent } from '@mui/material';
-import { React } from 'react';
-import styles from '../css/Dashboard.module.css'
+'use client';
+import { Box, Typography, AppBar, Toolbar, Grid, Card, CardContent, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import {React, useState} from 'react';
+import styles from '../css/Dashboard.module.css';
 
-function Dashboard(){
-    
+function Dashboard() {
+    const [repository, setRepository]  = useState('')
+    const [branch, setBranch]  = useState('')
+
+    {/**Should handle the change of the repository in the menu */}
+    const handleChangeRepo = () =>{
+        console.log("Repository")
+    }
+
+    {/**Should handle the change of the branch in the menu */}
+    const handleChangeBranch = () =>{
+        console.log("Branch")
+    }
+
     return (
         <Box className={styles.mainApp}>
             {/** Navbar at the top of the window */}
             <AppBar position="fixed" className={styles.topNavbar}>
-                <Toolbar sx={{ justifyContent: 'flex-start', pl:2}}>
+                <Toolbar sx={{ justifyContent: 'flex-start', pl: 2 }}>
                     <Typography>GitLogz</Typography>
                 </Toolbar>
             </AppBar>
+            
             {/** Menu bar handling the input of the repository and the branch */}
-            <AppBar position="fixed" className={styles.bottomNavbar}>
-                <Toolbar sx={{ justifyContent: 'flex-start', pl:2}}>
-                    <Typography>Repository</Typography>
-                    <Typography>Branch</Typography>
+            <Box className={styles.bottomNavbar}>
+                <Toolbar sx={{ justifyContent: 'flex-start', pl: 2 }}>
+                    {/**Drop down menu for users to choose the repo they would like to view */}
+                    <FormControl className={styles.dropDown} style={{minWidth:"20vw", marginRight:"25px"}}>
+                        <InputLabel className={styles.bootstrapInputLabel}>Repository</InputLabel>
+                        <Select
+                        value={repository}
+                        label="Repository"
+                        onChange={handleChangeRepo}
+                        >
+                        <MenuItem value={'A'}>A</MenuItem>
+                        <MenuItem value={'B'}>B</MenuItem>
+                        <MenuItem value={'C'}>C</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    {/**Drop down menu for users to input the branch they would like to view */}
+                    <FormControl className={styles.dropDown} style={{minWidth:"20vw"}}>
+                        <InputLabel className={styles.bootstrapInputLabel}>Branch</InputLabel>
+                        <Select
+                        value={branch}
+                        label="Branch"
+                        onChange={handleChangeBranch}
+                        >
+                        <MenuItem value={'A'}>A</MenuItem>
+                        <MenuItem value={'B'}>B</MenuItem>
+                        <MenuItem value={'C'}>C</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Toolbar>
-            </AppBar>
-            {/**Main dashboard area */}
+            </Box>
+            
+            {/** Main dashboard area */}
             <Box className={styles.analysis}>
-                <Typography>Dashboard</Typography>
-                {/**First to rows utilise a grid with 0 x spacing, bottom row is a separate box*/}
-                <Grid container>
+                <Typography className={styles.dashboardTitle}>Repository Dashboard</Typography>
+                {/**First two rows utilize a grid with no spacing, bottom row is a separate box*/}
+                <Grid container rowGap={10}  className={styles.gridContainer}>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Overall rating
-                                </Typography>
+                                <Typography>Overall rating</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Code coverage
-                                </Typography>
+                                <Typography>Code coverage</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Reliability
-                                </Typography>
+                                <Typography>Reliability</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Languages
-                                </Typography>
+                                <Typography>Languages</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Vulnerabilities
-                                </Typography>
+                                <Typography>Vulnerabilities</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Code issues
-                                </Typography>
+                                <Typography>Code issues</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Security hotspots
-                                </Typography>
+                                <Typography>Security hotspots</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Card>
+                        <Card className={styles.card}>
                             <CardContent>
-                                <Typography>
-                                    Known bugs
-                                </Typography>
+                                <Typography>Known bugs</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                 </Grid>
-                <Card>
+                <Card className={styles.kpiCard}>
                     <CardContent>
-                        <Typography>
-                            KPI GRAPH
-                        </Typography>
+                        <Typography>KPI summary</Typography>
+                        <Box className={styles.kpiGraph}></Box>
                     </CardContent>
                 </Card>
             </Box>
